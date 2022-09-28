b639e89fd61b9016f29a5ac48b85e1d0e98bfbd1
ascosper@hotmail.com <Amaya@Normas-Air.attlocal.net>
2022-09-27 19:17:31 -0500

    back end

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
+const { notes } = require('./db/db.json')
+const { title } = require('process')
+const PORT = process.env.PORT || 3001
 const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
+function findByTitle(title, notesArray){
+    const result = notesArray.filter(notes=> notes.title === title)[0]
+    return result
+}
+function createNewNote( body, notesArray) {
+    const note = body
+    notesArray.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: notesArray},null, 2)
+    )
+
+    return note
+}
+function validateNote(note){
+    if (!note.title || typeof note.title !== 'string') {
+        return false
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false
+    }
+}
 app.get('/api/notes', (req, res) => {
     
 let results = notes;
@@ -11,6 +37,27 @@ console.log(req.query)
 res.json(results)
 })
 
-app.listen(3001, () => {
+app.get('api/animals/:title', (req,res) => {
+    const result = findByTitle(req.params.title, notes)
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
+})
+
+app.post('/api/notes', (req, res) =>{
+    req.body.title = notes.legnth.toString()
+
+    if (!validateNote(req.body)){
+        res.status(400).send("the note is not properly formatted.")
+    }
+    else{
+        const note = createNewNote(req.body, notes)
+    res.json(note)
+}
+    
+})
+app.listen(PORT, () => {
     console.log( 'API server now on port')
 })
